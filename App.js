3a8cda090f089ae0ce70fe83e1f306524daa6605
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2025-12-09 20:56:22 -0300

    Aula 12 – Inserindo imagens para estilização

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 
 const App = () => {
@@ -7,9 +7,11 @@ const App = () => {
 
     	<View>
 
-			<Text style={{fontSize: 25, color: 'blue', margin: 20}}>
+			<Text style={{fontSize: 25, color: 'blue', marginTop: 40, textAlign: 'center'}}>
 				Olá mundo!
 			</Text>
+
+			<Image source={{uri: 'https://i.redd.it/l8chp2r5qe6a1.jpg'}} style={{width: 100, height: 100}}/>
 
     	</View>
 
