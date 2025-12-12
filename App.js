01422bc141b45bc622a6f7bbb652e64f4faf3fe7
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2025-12-12 08:40:41 -0300

    Aula 13 – Estilizando imagens

--- App.js
+++ App.js
@@ -5,13 +5,15 @@ import { View, Text, Image } from 'react-native';
 const App = () => {
 	return (
 
-    	<View>
+    	<View style={{justifyContent: 'center', alignItems: 'center'}}>
 
 			<Text style={{fontSize: 25, color: 'blue', marginTop: 40, textAlign: 'center'}}>
 				Olá mundo!
 			</Text>
 
-			<Image source={{uri: 'https://i.redd.it/l8chp2r5qe6a1.jpg'}} style={{width: 100, height: 100}}/>
+			<Image source={{uri: 'https://i.redd.it/l8chp2r5qe6a1.jpg'}} style={{width: 300, height: 300}}/>
+
+			<Image source={{uri: 'https://static.wikia.nocookie.net/gtawiki/images/b/bd/BigSmoke-GTASA.jpg/revision/latest?cb=20230629061252'}} style={{width: 300, height: 300, marginTop: 100}}/>
 
     	</View>
 
