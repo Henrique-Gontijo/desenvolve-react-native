156c0d729b65eeeeac0f5479ab810866eb889fd4
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2025-12-12 08:56:31 -0300

    Aula 14 – Scroll view e flat list

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, ScrollView, Text, Image, FlatList } from 'react-native';
 
 
 const App = () => {
@@ -7,13 +7,22 @@ const App = () => {
 
     	<View style={{justifyContent: 'center', alignItems: 'center'}}>
 
-			<Text style={{fontSize: 25, color: 'blue', marginTop: 40, textAlign: 'center'}}>
-				Olá mundo!
-			</Text>
+				<ScrollView>
+					
+				<Text style={{ fontSize: 25, color: 'blue', marginTop: 40, textAlign: 'center' }}>
+					Olá, meu chapa!
+				</Text>
 
-			<Image source={{uri: 'https://i.redd.it/l8chp2r5qe6a1.jpg'}} style={{width: 300, height: 300}}/>
+				<Image source={{uri: 'https://i.redd.it/l8chp2r5qe6a1.jpg' }} style={{width: 300, height: 300}}/>
 
-			<Image source={{uri: 'https://static.wikia.nocookie.net/gtawiki/images/b/bd/BigSmoke-GTASA.jpg/revision/latest?cb=20230629061252'}} style={{width: 300, height: 300, marginTop: 100}}/>
+				<FlatList data={[{key: 'Arroz'}, {key: 'Feijão'}, {key: 'Carne'}]} renderItem={({item}) => <Text>{item.key}</Text>}/>
+
+				<Image 
+					source={{ uri: 'https://static.wikia.nocookie.net/gtawiki/images/b/bd/BigSmoke-GTASA.jpg/revision/latest?cb=20230629061252' }} 
+					style={{ width: 300, height: 300, marginTop: 100 }}
+				/>
+
+			</ScrollView>
 
     	</View>
 
