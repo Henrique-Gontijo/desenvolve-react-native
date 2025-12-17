98b7171ff74a62ac0378ea8da6a31b0a744edc48
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2025-12-17 20:01:40 -0300

    Aula 22 – Conectando com o banco de dados

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { db } from './src/firebaseConnection.js';
 
 
 export default function Fire() {
