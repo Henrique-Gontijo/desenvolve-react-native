31a738a1fa4f1b313ca276b7848b8e2477f076e6
Henrique-Gontijo <henriquegontijo1507@gmail.com>
2025-12-24 14:50:25 -0300

    Aula 31 – Criando app Cronômetro (parte 1)

--- App.js
+++ App.js
@@ -1,120 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { doc, getDoc, addDoc, onSnapshot, query, collection, 
-	orderBy, limit} from 'firebase/firestore';
-import { db } from './src/firebaseConnection.js';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TOuchableOpacity, Image } from 'react-native';
 
 
-export default function App() {
+class App extends Component {
 
-	const [usuario, setUsuario] = useState({
-		nome: 'Carregando...',
-		email: 'Carregando...',
-		telefone: 'Carregando'
-	});
-	
-	const [novoNome, setNovoNome] = useState('');
-	const [novoEmail, setNovoEmail] = useState('');
-	const [novoTelefone, setNovoTelefone] = useState('');
+	render() {
+		return (
+			<View style={styles.container}>
 
-	useState('')
+				<Text style={styles.titulo}>Cronômetro</Text>
+				<Image source={require('./src/images/cj.jpg')} style={styles.crono}/>
+				<Text style={styles.tempo}>0.0</Text>
 
-	useEffect(() => {
-		const q = query(collection(db, 'usuario'),
-			orderBy('nome', 'desc'), limit(1)
+			</View>
 		);
-
-		const unsubscribe = onSnapshot(q, (querySnapshot) => {
-			if (!querySnapshot.empty) {
-				setUsuario(querySnapshot.docs[0].data());
-			} else {
-				console.log('Nenhum usuário encontrado!');
-			}
-		});
-		return () => unsubscribe();
-	}, []);
-
-	// Adicionar usuário
-	async function handleRegister() {
-		if (novoNome === '' || novoEmail === '' || novoTelefone === '') {
-			alert('Preencha todos os Campos');
-			return;
-		}
-
-		try {
-			await addDoc(collection(db, 'usuario'), {
-				nome: novoNome,
-				email: novoEmail,
-				telefone: novoTelefone
-			});
-
-			console.log('Usuário registrado com sucesso!');
-
-			//Limpar os campos após cadastro
-			setNovoNome('');
-			setNovoEmail('');
-			setNovoTelefone('');
-		} catch (error) {
-			console.error(
-				'"Puts, deu zebra!"\n\n Ocorreu algum erro ao registrar o novo usuário, veja: \n\n', error
-			);
-		}
-	}
-
-	return (
-
-		<View style={styles.container}> 
-			<Text style={{fontSize: 24}}>Último cadastro: </Text>
-
-			<Text style={{fontSize: 24}}>Nome: {usuario.nome}</Text>
-			<Text style={{fontSize: 24}}>E-mail: {usuario.email}</Text>
-			<Text style={{fontSize: 24}}>Telefone: {usuario.telefone}</Text>
-
-			<TextInput style={styles.input} placeholder='Nome' 
-				value={novoNome} onChangeText={setNovoNome}/>
-			<TextInput style={styles.input} placeholder='E-mail' 
-				value={novoEmail} onChangeText={setNovoEmail} keyboardType='email-adress'/>
-			<TextInput style={styles.input} placeholder='Telefone' 
-				value={novoTelefone} onChangeText={setNovoTelefone} keyboardType='numeric'/>
-
-			<TouchableOpacity style={styles.button} onPress={handleRegister}>
-				<Text style={styles.buttonText}>Adicionar</Text>
-			</TouchableOpacity>
-			
-		</View>
-
-	);
+	} 
 }
+
 
 const styles = StyleSheet.create({
 
 	container: {
-		flex: 1,
-		paddingTop: 40,
-		justifyContent: 'center',
-		alignItems: 'center',
+
+	},
+	titulo: {
+
+	},
+	crono: {
+
+	},
+	tempo: {
+
 	},
 
-	input: {
-		width: '80%',
-		height: 40,
-		padding: 8,
-		marginTop: 16,
-		borderWidth: 1,
-		borderRadius: 8,
-		borderColor: 'grey',
-	},
+});
 
-	button: {
-		backgroundColor: '#000',
-		padding: 6,
-		marginTop: 16,
-		borderRadius: 8,
-	},
-
-	buttonText: {
-		color: '#FFF',
-		padding: 4,
-	},
-
-}); 
+export default App;
